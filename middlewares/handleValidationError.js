4b5b93dc3246e900b49e8ef9d1013853f2f235ae
AlexButeau <alexbuteau1@gmail.com>
2020-12-21 13:37:20 +0100

    merged with dev

--- middlewares/handleValidationError.js
+++ middlewares/handleValidationError.js
@@ -4,7 +4,7 @@ const { ValidationError } = require('../error-types');
 module.exports = (error, req, res, next) => {
   if (error instanceof ValidationError)
     return res.status(422).send({
-      errorMessage: error.message,
+      errorMessage: error.errorsByField[0].message,
       ...error,
     });
   return next(error);
