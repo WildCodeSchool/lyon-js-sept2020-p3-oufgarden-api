ef7c95f0b09a4dc8d75236404b9323bff4346fc4
AlexButeau <alexbuteau1@gmail.com>
2021-01-06 11:31:42 +0100

    fixed login

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,9 @@ const adminLogin = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const requireRequestBody = require('../middlewares/requireRequestBody');
 
-const { handleLogin, handleLogout } = require('../controllers/login.js');
+const { handleLoginAdmin, handleLogout } = require('../controllers/login.js');
 
-adminLogin.post('/', requireRequestBody, asyncHandler(handleLogin));
+adminLogin.post('/', requireRequestBody, asyncHandler(handleLoginAdmin));
 adminLogin.get('/', asyncHandler(handleLogout));
 
 module.exports = { adminLogin };
